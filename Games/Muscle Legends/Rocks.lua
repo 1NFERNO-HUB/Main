48cbdc74fcdc11343d8713484f63330bde4b5748
1NFERNO-HUB <havocxsherya@gmail.com>
2025-11-21 17:48:30 -0600

    Create Rocks.lua

---
+++ Games/Muscle Legends/Rocks.lua
@@ -0,0 +1,58 @@
+local RockData = {
+
+    {
+        displayName = "Tiny Rock",
+        internalName = "Tiny Island Rock",
+        neededDurability = 0,
+    },
+
+    {
+        displayName = "Starter Rock",
+        internalName = "Starter Island Rock",
+        neededDurability = 100,
+    },
+
+    {
+        displayName = "Legend Beach Rock",
+        internalName = "Legend Beach Rock",
+        neededDurability = 5000,
+    },
+
+    {
+        displayName = "Frozen Rock",
+        internalName = "Frost Gym Rock",
+        neededDurability = 150000,
+    },
+
+    {
+        displayName = "Mythical Rock",
+        internalName = "Mythical Gym Rock",
+        neededDurability = 400000,
+    },
+
+    {
+        displayName = "Eternal Rock",
+        internalName = "Eternal Gym Rock",
+        neededDurability = 750000,
+    },
+
+    {
+        displayName = "Legend Rock",
+        internalName = "Legend Gym Rock",
+        neededDurability = 1000000,
+    },
+
+    {
+        displayName = "Muscle King Rock",
+        internalName = "Muscle King Gym Rock",
+        neededDurability = 5000000,
+    },
+
+    {
+        displayName = "Jungle Rock",
+        internalName = "Ancient Jungle Rock",
+        neededDurability = 10000000,
+    },
+}
+
+return RockData
