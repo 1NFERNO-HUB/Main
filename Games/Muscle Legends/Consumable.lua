9d6f9d6cbb82e22e36951b6a6fd19a6885129747
1NFERNO-HUB <havocxsherya@gmail.com>
2025-11-21 17:57:57 -0600

    Create Consumable.lua

---
+++ Games/Muscle Legends/Consumable.lua
@@ -0,0 +1,12 @@
+local ConsumableData = {
+   "Energy Bar",
+    "Energy Shake",
+    "Protein Bar",
+    "Protein Shake",
+    "TOUGH Bar",
+    "ULTRA Shake",
+    "Protein Egg",
+    "Tropical Shake"
+}
+
+return ConsumableData
