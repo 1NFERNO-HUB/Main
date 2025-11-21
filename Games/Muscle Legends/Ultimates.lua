58325f034a929320ad7c9f1e9dfb8aac954b9aa9
1NFERNO-HUB <havocxsherya@gmail.com>
2025-11-21 17:46:40 -0600

    Create Ultimates.lua

---
+++ Games/Muscle Legends/Ultimates.lua
@@ -0,0 +1,15 @@
+local Ultimates = {
+    "+1 Daily Spin",
+    "+1 Pet Slot",
+    "+10 Item Capacity",
+    "+5% Rep Speed",
+    "Demon Damage",
+    "Galaxy Gains",
+    "Golden Rebirth",
+    "Jungle Swift",
+    "Muscle Mind",
+    "x2 Chest Rewards",
+    "x2 Quest Rewards"
+}
+
+return Ultimates
