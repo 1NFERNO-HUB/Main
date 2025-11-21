9d815b23deb743823eca0198aa190f0784c443c9
1NFERNO-HUB <havocxsherya@gmail.com>
2025-11-21 17:55:28 -0600

    Create Chest.lua

---
+++ Games/Muscle Legends/Chest.lua
@@ -0,0 +1,11 @@
+local ChestData = {
+    "Enchanted Chest",
+    "Golden Chest",
+    "Jungle Chest",
+    "Legends Chest",
+    "Magma Chest",
+    "Mythical Chest",
+    "groupRewards"
+}
+
+return ChestData
