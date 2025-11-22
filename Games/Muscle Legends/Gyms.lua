c4b8daef942dfeed81e51b0f2d48681366c25523
1NFERNO-HUB <havocxsherya@gmail.com>
2025-11-21 21:47:46 -0600

    Create Gyms.lua

---
+++ Games/Muscle Legends/Gyms.lua
@@ -0,0 +1,15 @@
+local TeleportLocations = {
+    ["Spawn"] = "CFrame.new(2, 8, 115)",
+    ["Secret Area"] = "CFrame.new(1947, 2, 6191)",
+    ["Tiny Gym"] = "CFrame.new(-34, 7, 1903)",
+    ["Frozen Gym"] = "CFrame.new(-2600.00244, 3.67686558, -403.884369)",
+    ["Mythical Gym"] = "CFrame.new(2255, 7, 1071)",
+    ["Hell Gym"] = "CFrame.new(-6768, 7, -1287)",
+    ["Legend Gym"] = "CFrame.new(4604, 991, -3887)",
+    ["Muscle King Gym"] = "CFrame.new(-8646, 17, -5738)",
+    ["Jungle Gym"] = "CFrame.new(-8659, 6, 2384)",
+    ["Brawl Lava"] = "CFrame.new(4471, 119, -8836)",
+    ["Brawl Desert"] = "CFrame.new(960, 17, -7398)",
+    ["Brawl Regular"] = "CFrame.new(-1849, 20, -6335)",
+}
+return TeleportLocations
