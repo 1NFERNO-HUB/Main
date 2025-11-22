2ba29dae7912269c69271ef6cca41503b205787a
1NFERNO-HUB <havocxsherya@gmail.com>
2025-11-21 21:50:55 -0600

    Create Codes.lua

---
+++ Games/Muscle Legends/Codes.lua
@@ -0,0 +1,18 @@
+local Codes = {
+  "junglegym500",
+  "epicmuscle20",
+  "ultimate250",
+  "mightygems2500",
+  "SkyAgility50",
+  "SpaceGems50",
+  "Speedy50",
+  "SuperMuscle100",
+  "SuperPunch100
+  "MegaLift50",
+  "MillionWarriors",
+  "MuscleStorm50",
+  "Luanch250",
+  "GalaxyCrystal50",
+  "FrostGems10",
+  "EpicReward500",
+return Codes
